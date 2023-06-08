783408cb6275513427860ea52728bad9a3456b8d
linuxra <54610196+linuxra@users.noreply.github.com>
2023-06-08 14:37:26 -0400

    Update draw_plots.py

--- draw_plots.py
+++ draw_plots.py
@@ -219,4 +219,136 @@ df = pd.DataFrame({
 obj = MyClass(title='My Bubble Plot')
 obj.draw_plots(df)
 
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
+class MyClass:
+    def __init__(self, title):
+        self.title = title
+
+    def draw_plots(self, df):
+        # Normalize the values for bubble size
+        scaler = MinMaxScaler()
+        df[df.columns[2]] = scaler.fit_transform(df[df.columns[2]].values.reshape(-1,1)) * 1000
+
+        # Bubble plot for all data
+        plt.figure(figsize=(10, 10))
+        sns.scatterplot(data=df, x=df.columns[1], y=df.columns[2], hue=df.columns[0], size=df.columns[2], legend="full", sizes=(50, 500))
+        plt.title(self.title)
+
+        plt.show()
+
+# Assuming you have a dataframe df
+df = pd.DataFrame({
+    'group': ['A', 'A', 'B', 'B', 'A', 'A', 'B', 'B'],
+    'time_period': [1, 2, 1, 2, 3, 4, 3, 4],
+    'value': [6000, 7000, 8000, 9000, 6000, 7000, 8000, 9000]
+})
+
+obj = MyClass(title='My Bubble Plot')
+obj.draw_plots(df)
+
+
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+class MyClass:
+    def __init__(self, title):
+        self.title = title
+
+    def draw_plots(self, df):
+        # Normalize the values for bubble size
+        scaler = MinMaxScaler()
+        df[df.columns[2]] = scaler.fit_transform(df[df.columns[2]].values.reshape(-1,1)) * 1000
+
+        # Bubble plot for all data
+        plt.figure(figsize=(10, 10))
+        sns.scatterplot(data=df, x=df.columns[1], y=df.columns[2], hue=df.columns[0], size=df.columns[2], palette="Pastel1", legend="full", sizes=(50, 500))
+        plt.title(self.title)
+
+        plt.show()
+
+# Assuming you have a dataframe df
+df = pd.DataFrame({
+    'group': ['A', 'A', 'B', 'B', 'A', 'A', 'B', 'B'],
+    'time_period': [1, 2, 1, 2, 3, 4, 3, 4],
+    'value': [6000, 7000, 8000, 9000, 6000, 7000, 8000, 9000]
+})
+
+obj = MyClass(title='My Bubble Plot')
+obj.draw_plots(df)
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+# List of metrics and time periods
+metrics = ['A', 'B', 'C', 'D', 'E', 'F']
+time_periods = ['t2021Q1', 't2021Q2', 't2021Q3', 't2021Q4', 't2022Q1', 't2022Q2', 't2022Q3', 't2022Q4']
+
+# Create a DataFrame with random data
+np.random.seed(0)  # for reproducible results
+data = np.random.rand(len(metrics), len(time_periods))
+df = pd.DataFrame(data, index=metrics, columns=time_periods)
+
+# Create a heatmap
+sns.heatmap(df, cmap='viridis')
+plt.title('Heatmap of Metrics over Time Periods')
+plt.show()
+
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib.gridspec import GridSpec
+from matplotlib.colors import LinearSegmentedColormap
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class MyClass:
+    def __init__(self, title):
+        self.title = title
+
+    def draw_plots(self, df):
+        fig = plt.figure(figsize=(20, 15))
+        gs = GridSpec(2, 3, figure=fig)
+
+        # Create a colormap for the unique groups
+        colormap = plt.cm.get_cmap('viridis', len(df[df.columns[0]].unique()))
+
+        # Normalize the values for bubble size
+        scaler = MinMaxScaler()
+        values_scaled = scaler.fit_transform(df[df.columns[2]].values.reshape(-1,1)) * 1000
+
+        # Bubble plot
+        scatter = fig.add_subplot(gs[0, 2])
+        scatter.scatter(x=df[df.columns[1]], y=df[df.columns[2]],
+                        s=values_scaled,
+                        c=df[df.columns[0]].astype('category').cat.codes, cmap=colormap, alpha=0.5)
+
+        # Create a legend for the colors
+        colorbar = plt.colorbar(scatter, ticks=range(len(df[df.columns[0]].unique())), ax=scatter)
+        colorbar.set_label(df.columns[0])
+        colorbar.set_ticks([i + 0.5 for i in range(len(df[df.columns[0]].unique()))])
+        colorbar.set_ticklabels(df[df.columns[0]].unique())
+
+        # Line plot
+        lineplot = fig.add_subplot(gs[1, :])
+        sns.lineplot(data=df, x=df.columns[1], y=df.columns[2], hue=df.columns[0], ax=lineplot)
+
+        # Stacked bar plot
+        df_pivot = df.pivot(index=df.columns[1], columns=df.columns[0], values=df.columns[2])
+        barplot = fig.add_subplot(gs[0, 0])
+        df_pivot.plot(kind='bar', stacked=True, ax=barplot)
+
+        # Define a custom colormap
+        c_map = LinearSegmentedColormap.from_list('mycmap', ['yellow', 'green'])
+
+        # Heatmap
+        heatmap = fig.add_subplot(gs[0, 1])
+        sns.heatmap(df_pivot, cmap=c_map, ax=heatmap)
+
+        # Set the title of the figure
+        fig.suptitle(self.title)
+
+        plt.tight_layout()
+        plt.show()
