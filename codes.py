ea2916b5bbe10fa97e6da1bf14e60529eed72e69
linuxra <54610196+linuxra@users.noreply.github.com>
2023-06-22 12:36:52 -0400

    Update codes.py

--- codes.py
+++ codes.py
@@ -16,3 +16,50 @@ for col in ['r1', 'r2', 'r3', 'r4']:
 
 # Display the modified DataFrame
 print(df1)
+
+
+WITH unpivoted AS (
+    SELECT
+        id,
+        cb_cd,
+        CASE col
+            WHEN 'r1' THEN r1
+            WHEN 'r2' THEN r2
+            WHEN 'r3' THEN r3
+            WHEN 'r4' THEN r4
+        END AS r,
+        col
+    FROM
+        table1
+    CROSS JOIN (
+        SELECT 'r1' AS col
+        UNION ALL SELECT 'r2'
+        UNION ALL SELECT 'r3'
+        UNION ALL SELECT 'r4'
+    ) AS columns
+),
+joined AS (
+    SELECT
+        u.id,
+        u.col,
+        CASE
+            WHEN u.cb_cd = 'TU' THEN t2.TU_rsn_cd
+            WHEN u.cb_cd = 'EX' THEN t2.EXP_rsn_cd
+            WHEN u.cb_cd = 'EF' THEN t2.EFX_rsn_cd
+            ELSE u.r
+        END AS r_new
+    FROM
+        unpivoted u
+    LEFT JOIN
+        table2 t2 ON u.r = t2.r
+)
+SELECT
+    id,
+    MAX(CASE WHEN col = 'r1' THEN r_new END) AS r1,
+    MAX(CASE WHEN col = 'r2' THEN r_new END) AS r2,
+    MAX(CASE WHEN col = 'r3' THEN r_new END) AS r3,
+    MAX(CASE WHEN col = 'r4' THEN r_new END) AS r4
+FROM
+    joined
+GROUP BY
+    id;
